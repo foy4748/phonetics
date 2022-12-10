bcf991364465b639f71519ab310a1229d0deada7
TH-Raju <rajukhan894200@gmail.com>
2022-12-11 00:09:02 +0600

    Premium Card Design Updated

--- components/pricingComponents/PremiumCard.jsx
+++ components/pricingComponents/PremiumCard.jsx
@@ -6,18 +6,18 @@ const PremiumCard = () => {
             <div>
                 <div className="card w-96 bg-base-100 shadow-xl">
                     <div className="card-body">
-                        <h2 className="card-title">Free Course</h2>
+                        <h2 className="card-title text-2xl font-bold">Premium Course</h2>
                         <ul className="steps steps-vertical lg:steps-horizontal">
                             <li className="step step-primary">Live Class</li>
                             <li className="step step-primary">Class Recorded</li>
                             <li className="step step-primary">Live Mentor Support</li>
                             <li className="step step-primary">16 Assignment</li>
-                            <li className="step step-primary">20 Hand&apos;s on Projects</li>
-                            <li className="step step-primary">3 Month Internship</li>
-                            <li className="step step-primary">Interview Preparation</li>
+                            <li className="step step-primary">16 Hand&apos;s on Projects</li>
+                            <li className="step"><span className='opacity-50'>3 Month Internship</span></li>
+                            <li className="step"><span className='opacity-50'>Interview Preparation</span></li>
                             <li className="step step-primary">Certificate</li>
                             <li className="step step-primary">Life Time Access</li>
-                            <li className="step step-primary">Helping tools and Notes</li>
+                            <li className="step step-primary">Helping and Notes</li>
                         </ul>
                         <div className="card-actions justify-end">
                             <button className="btn btn-primary">Enroll Now</button>
