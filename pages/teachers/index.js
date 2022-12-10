fe7e0520e10a7a8dad43c1855c7dae4b5c4eb1a5
Rajib Sarker <redhat0593@gmail.com>
2022-12-10 21:26:44 +0600

    test commit

--- pages/teachers/index.js
+++ pages/teachers/index.js
@@ -2,7 +2,7 @@ import TeacherCard from "../../components/teachersComponents/TeacherCard";
 export default function Lessons() {
   return (
     <div>
-      <h1>I am Teachers</h1>
+      <h1>I am Teachers well done</h1>
       <TeacherCard />
     </div>
   );
