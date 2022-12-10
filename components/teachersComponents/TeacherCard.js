8f2d7d7cee8071a52b20c4a85a2c4ec31770ed07
Rajib Sarker <redhat0593@gmail.com>
2022-12-10 23:29:45 +0600

    cards creaded

--- components/teachersComponents/TeacherCard.js
+++ components/teachersComponents/TeacherCard.js
@@ -1,7 +1,28 @@
-export default function TeacherCard() {
+import Image from "next/image";
+
+export default function TeacherCard({ teacher }) {
+  const { name, desc, sub, Country, image, email } = teacher;
+
   return (
-    <div>
-      <h1>I am a card</h1>
+    <div className="border p-2 rounded-lg"
+      style={{ boxShadow: "0px 5px 15px 2px #764cd866" }}
+    >
+      <div
+        className="rounded-lg p-1 bg-gradient-to-r from-purple-500 via-purple-400 to-indigo-600"
+      >
+        <div className="bg-white p-2 rounded">
+          <div className="image rounded-lg overflow-hidden">
+            <Image src={image} alt='card-imaa' />
+          </div>
+          <div className="text">
+            <h2>{name}</h2>
+            <p>{desc}</p>
+            <p>{sub}</p>
+            <p>{Country}</p>
+            <p>{email}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/teachers/index.js
+++ pages/teachers/index.js
@@ -1,9 +1,120 @@
+import Image from "next/image";
 import TeacherCard from "../../components/teachersComponents/TeacherCard";
+import image1 from "../../Images/how-to-become-a-language-teacher.jpg";
+
 export default function Lessons() {
+
+  const teacherList = [
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    },
+
+    {
+      name: "English",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, maxime.",
+      sub: "Japanese",
+      Country: "japan",
+      image: image1, 
+      email: "e@email.com",
+    }
+
+  ]
+
   return (
-    <div>
+    <div className="max-w-screen-xl mx-auto">
       <h1>I am Teachers well done</h1>
-      <TeacherCard />
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+
+        {
+          teacherList.map((teacher, i) => <TeacherCard key={i} teacher={teacher}/>)
+        }
+      </div>
     </div>
   );
 }
