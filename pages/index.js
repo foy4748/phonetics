d90b5ecf25b58a65c4126e4575f08076bc2860fa
unknown <mosharafctg111@gmail.com>
2022-12-11 17:56:42 +0600

    added the homepage and more function

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/homeComponents/Footer'
+import Header from '../components/homeComponents/Header'
+import LanguageCard from '../components/homeComponents/LanguageCard'
+import LearningFeature from '../components/homeComponents/LearningFeature'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h2 className='text-5xl text-info'>hello world</h2>
-      
+    <div className='mx-auto md:max-w-[1240px]'>
+      <Header></Header>
+      <LanguageCard></LanguageCard>
+      <LearningFeature></LearningFeature>
+      <Footer></Footer>
     </div>
   )
 }
